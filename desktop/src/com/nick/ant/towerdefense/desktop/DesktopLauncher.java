3ac65803dc8849106848d2419567cf606a03e35a
Nicholas Hope <nicktoony@gmail.com>
2014-08-04 23:26:32 +0100

    Initial Commit

---
+++ desktop/src/com/nick/ant/towerdefense/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nick.ant.towerdefense.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nick.ant.towerdefense.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
