fe90d3c508bfa4bc17b0c05fa38617cd2fdad029
HGreen <harry@hgreen.co.uk>
2014-09-14 15:49:09 +0100

    Added UserPlayer and basic keyboard control

---
+++ core/src/com/nick/ant/towerdefense/renderables/entities/players/UserPlayer.java
@@ -0,0 +1,49 @@
+package com.nick.ant.towerdefense.renderables.entities.players;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by hgreen on 14/09/14.
+ */
+public class UserPlayer extends Player{
+
+    public UserPlayer(int x, int y){
+        super(x, y);
+    }
+
+    @Override
+    public void render(SpriteBatch spriteBatch){
+
+        if(Gdx.input.isKeyPressed(Input.Keys.W)){
+            this.moveUp = true;
+        }else{
+            this.moveUp = false;
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.S)){
+            this.moveDown = true;
+        }else{
+            this.moveDown = false;
+        }
+
+
+        if(Gdx.input.isKeyPressed(Input.Keys.A)){
+            this.moveLeft = true;
+        }else{
+            this.moveLeft = false;
+        }
+
+
+        if(Gdx.input.isKeyPressed(Input.Keys.D)){
+            this.moveRight = true;
+        }else{
+            this.moveRight = false;
+        }
+
+
+        super.render(spriteBatch);
+    }
+
+}
