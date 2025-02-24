70326ae1a1e0d784c80b26e5473b0ef584b31a3c
Nick Hope <nicktoony@gmail.com>
2025-02-24 01:05:57 +0100

    feat: travis replaced with github actions

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM openjdk:8-jdk
+
+WORKDIR /app
+
+COPY . .
+
+RUN chmod +x gradlew
+
+RUN ./gradlew desktop:dist
+RUN cp -R android/assets desktop/build/libs/
+
+RUN ./gradlew server:dist
+RUN cp -R android/assets server/build/libs/
+
+RUN ./gradlew html:dist
---
+++ legacybuild.sh
@@ -0,0 +1,24 @@
+# Remove existing build
+rm -r dist
+mkdir -p dist
+
+# Build all flavours
+docker build -t cstopdown . 
+
+# Create a dummy container layer to pull files from
+docker create --name dummy cstopdown
+docker cp dummy:/app/html/build/dist ./dist/html
+docker cp dummy:/app/desktop/build/libs ./dist/desktop
+docker cp dummy:/app/server/build/libs ./dist/server
+# Cleanup the dummy layer
+docker rm -f dummy
+
+# Cleanup the build image
+# docker image rm cstopdown
+
+docker build -f Dockerfile.server -t cstopdown:server-latest .
+docker image tag cstopdown:server-latest nicktoony/cstopdown:server-latest
+docker image push nicktoony/cstopdown:server-latest
+docker image rm cstopdown:server-latest
+
+# docker run --rm -it --entrypoint bash cstopdown
